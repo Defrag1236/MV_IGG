927207a1e8f866098b6c8e9030856e6b91a9e534
zlobin <defrag12@gmail.com>
2020-04-06 22:12:19 +0700

    Add new script

--- 04_do_table_with_additional_results_of_mv_for_replicated_snps.R
+++ 04_do_table_with_additional_results_of_mv_for_replicated_snps.R
@@ -1,1 +1,88 @@
-123
+### do table with additional results of mv for replicated snps ###
+
+# extract info 
+
+idnames_mv <- c("/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_N_glycosylation.Rdata",
+				"/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_bisectingGlcNAc.Rdata")
+
+
+names_mv <- c("multi_N_glycosylation", "multi_bisectingGlcNAc")
+
+snps <- matrix(ncol=2, nrow=5) 
+
+
+snps[,1] <- c("rs11895615", "rs1372288", "rs12635457", "rs479844", "rs4561508")
+snps[,2] <- c("multi_bisectingGlcNAc", "multi_N_glycosylation", "multi_N_glycosylation", "multi_N_glycosylation", "multi_N_glycosylation")
+
+colnames(snps) <- c("SNP", "trait")
+snps <- as.data.frame(snps)
+
+extracted_data <- list()
+names(extracted_data) <- paste(snps[,1], "scan", sep="_")
+
+
+for (n in 1:2) {
+
+	load(idnames_mv[n])
+	eval(expr=parse(text=paste("MV_P=",names_mv[n])))
+	x <- names_mv[n]
+	eval(expr=parse(text=paste("rm(",names_mv[n],")")))
+
+		for (i in (1:nrow(snps))) {
+			
+			if (x==snps$trait[i]) {
+
+				extracted_data[[i]] <- MV_P$scan[(which(snps$SNP[i]==MV_P$scan[,1])),-c(2:6)]
+
+
+				print(paste(n, i, sep=":")) 
+					
+			} 
+		
+		}
+
+	print(n)	
+	}
+
+
+extracted_data_coef <- list()
+names(extracted_data_coef) <- paste(snps[,1], "coef", sep="_")
+
+for (n in 1:2) {
+
+	load(idnames_mv[n])
+	eval(expr=parse(text=paste("MV_P=",names_mv[n])))
+	x <- names_mv[n]
+	eval(expr=parse(text=paste("rm(",names_mv[n],")")))
+
+	coef <- MV_P$coef
+	rownames(coef) <- MV_P$scan[,1]
+
+		for (i in (1:nrow(snps))) {
+			
+			if (x==snps$trait[i]) {
+
+				extracted_data_coef[[i]] <- coef[(which(snps$SNP[i]==rownames(coef))),-c(2:6)]
+
+
+				print(paste(n, i, sep=":")) 
+					
+			} 
+		
+		}
+
+	print(n)	
+	}
+
+
+# save results 
+
+for (n in (1:5)) {
+
+	path1 <- paste("/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/coefficients_and_other_info/", names(extracted_data)[n], ".txt", sep="")
+	path2 <- paste("/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/coefficients_and_other_info/", names(extracted_data_coef)[n], ".txt", sep="")
+
+	write.table(extracted_data[[n]], path1, col.names=T, row.names=F, quote=F)
+	write.table(t(as.matrix(extracted_data_coef[[n]])), path2, col.names=T, row.names=F, quote=F)
+
+	}
