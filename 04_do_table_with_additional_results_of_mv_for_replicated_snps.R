e77d2a33e2fc7f958bbeb906a0ca9d772521190d
zlobin <defrag12@gmail.com>
2020-04-10 20:41:07 +0700

    Update script

--- 04_do_table_with_additional_results_of_mv_for_replicated_snps.R
+++ 04_do_table_with_additional_results_of_mv_for_replicated_snps.R
@@ -3,25 +3,27 @@
 # extract info 
 
 idnames_mv <- c("/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_N_glycosylation.Rdata",
-				"/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_bisectingGlcNAc.Rdata")
+				"/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_bisectingGlcNAc.Rdata",
+				"/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_fucosylation.Rdata",
+				"/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/Rdata/multi_sialylation.Rdata")
 
 
-names_mv <- c("multi_N_glycosylation", "multi_bisectingGlcNAc")
+names_mv <- c("multi_N_glycosylation", "multi_bisectingGlcNAc", "multi_fucosylation", "multi_sialylation")
 
-snps <- matrix(ncol=2, nrow=5) 
+snps <- matrix(ncol=2, nrow=7) 
 
 
-snps[,1] <- c("rs11895615", "rs1372288", "rs12635457", "rs479844", "rs4561508")
-snps[,2] <- c("multi_bisectingGlcNAc", "multi_N_glycosylation", "multi_N_glycosylation", "multi_N_glycosylation", "multi_N_glycosylation")
+snps[,1] <- c("rs11895615", "rs1372288", "rs12635457", "rs479844", "rs4561508", "rs7257072", "rs2745851")
+snps[,2] <- c("multi_bisectingGlcNAc", "multi_N_glycosylation", "multi_N_glycosylation", "multi_N_glycosylation", "multi_N_glycosylation", "multi_fucosylation", "multi_sialylation")
 
 colnames(snps) <- c("SNP", "trait")
 snps <- as.data.frame(snps)
 
-extracted_data <- list()
+extracted_data <- vector("list", nrow(snps))
 names(extracted_data) <- paste(snps[,1], "scan", sep="_")
 
 
-for (n in 1:2) {
+for (n in 1:4) {
 
 	load(idnames_mv[n])
 	eval(expr=parse(text=paste("MV_P=",names_mv[n])))
@@ -45,10 +47,10 @@ for (n in 1:2) {
 	}
 
 
-extracted_data_coef <- list()
+extracted_data_coef <- vector("list", nrow(snps))
 names(extracted_data_coef) <- paste(snps[,1], "coef", sep="_")
 
-for (n in 1:2) {
+for (n in 1:4) {
 
 	load(idnames_mv[n])
 	eval(expr=parse(text=paste("MV_P=",names_mv[n])))
@@ -62,7 +64,7 @@ for (n in 1:2) {
 			
 			if (x==snps$trait[i]) {
 
-				extracted_data_coef[[i]] <- coef[(which(snps$SNP[i]==rownames(coef))),-c(2:6)]
+				extracted_data_coef[[i]] <- coef[(which(snps$SNP[i]==rownames(coef))),]
 
 
 				print(paste(n, i, sep=":")) 
@@ -77,7 +79,7 @@ for (n in 1:2) {
 
 # save results 
 
-for (n in (1:5)) {
+for (n in (1:7)) {
 
 	path1 <- paste("/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/coefficients_and_other_info/", names(extracted_data)[n], ".txt", sep="")
 	path2 <- paste("/home/common/projects/Multivariate_analysis_IgG/2020_work_for_paper/results/coefficients_and_other_info/", names(extracted_data_coef)[n], ".txt", sep="")
